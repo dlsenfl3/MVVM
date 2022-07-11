ed10a2d3350176260ff47ca880574c2b8f40c5f8
KIS_VHD <dlsenfl3@gmail.com>
2022-07-11 19:47:45 +0900

    RelayCommand

---
+++ MVVM_Example/Commands/ConvertSecondViewCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Example.Commands
+{
+    public class ConvertSecondViewCommand : CommandBase
+    {
+
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MVVM_Example/Commands/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Example.Commands
+{
+    public class RelayCommand : CommandBase
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentException("execute");
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
