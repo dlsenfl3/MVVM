8138ca02835f6df02cf6138e294fac3574126ae4
KIS_VHD <dlsenfl3@gmail.com>
2022-07-20 14:20:50 +0900

    Basics_Views

---
+++ MVVM_Basics/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Basics.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ViewModelBase CurrentViewModel { get; set; }
+        public MainWindowViewModel()
+        {
+            CurrentViewModel = new AViewModel();
+        }
+    }
+}
